8beca8f696ddcb7b7367593fa6910330420c9c47
jpnoro2003 <jpnoro2003@gmail.com>
2023-11-20 14:32:49 -0500

    Added Sudoku page

---
+++ sudoku/grid_creation.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    // Creating a table dynamically
+    var table = $('<table class="table-responsive">');
+
+    for(var g=0; g<3; g++){ // major square row
+        var sq_row = $('<tr class="bg-transparent">');
+
+        for(var h=0; h<3; h++){ // major square col
+            var sq_cell = $('<th scope="col" class="bg-transparent" style="border:5px solid transparent">');
+
+            for(var i=1; i<4; i++){ // minor square row
+                var row = $('<tr class="bg-transparent">');
+                
+                for(var j=1; j<4; j++){ // minor square col
+                    var cell = $('<th scope="col" class="bg-transparent">');
+                    var field = $('<input id="'+[3*g+i, 3*h+j].toString()+'"style="height:7vh; font-size: 4vh; border-radius: 0" class="form-control text-center" maxlength="1" oninput="this.value=this.value.replace(/[^1-9]/g,'+'\'\''+');">');
+                    cell.append(field);
+                    row.append(cell);
+                }
+            
+                sq_cell.append(row);
+            }
+            sq_row.append(sq_cell)
+        }
+        table.append(sq_row)
+    }
+    // Appending the table to the HTML body
+    $('div.container').append(table);
+});
