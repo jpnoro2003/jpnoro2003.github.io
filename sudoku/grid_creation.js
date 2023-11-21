ffe7eb0dc88ada6cc7444b835dfccfbee3b817fa
jpnoro2003 <jpnoro2003@gmail.com>
2023-11-20 21:53:15 -0500

    added test files

--- sudoku/grid_creation.js
+++ sudoku/grid_creation.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
         var sq_row = $('<tr class="bg-transparent">');
 
         for(var h=0; h<3; h++){ // major square col
-            var sq_cell = $('<th scope="col" class="bg-transparent" style="border:1vh solid transparent;">');
+            var sq_cell = $('<th scope="col" class="bg-transparent" style="border:1vh solid transparent; aspect-ratio: 1 / 1">');
 
             for(var i=1; i<4; i++){ // minor square row
                 var row = $('<tr class="bg-transparent">');
                 
                 for(var j=1; j<4; j++){ // minor square col
-                    var cell = $('<th scope="col" class="bg-transparent">');
+                    var cell = $('<th scope="col" class="bg-transparent" style="aspect-ratio: 1 / 1">');
                     var field = $('<input id="'+[3*g+i, 3*h+j].toString()+'"style="height:7vh; font-size: 3.5vh; border-radius: 0;" class="form-control text-center" maxlength="1" oninput="this.value=this.value.replace(/[^1-9]/g,'+'\'\''+');">');
                     cell.append(field);
                     row.append(cell);
