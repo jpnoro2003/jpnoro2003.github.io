bd89756d5374377559ba075a948aa788307359cd
jpnoro2003 <jpnoro2003@gmail.com>
2023-11-21 00:00:00 -0500

    fixed grid scaling

--- sudoku/grid_creation.js
+++ sudoku/grid_creation.js
@@ -7,14 +7,14 @@ $(document).ready(function(){
         var sq_row = $('<tr class="bg-transparent">');
 
         for(var h=0; h<3; h++){ // major square col
-            var sq_cell = $('<th scope="col" class="bg-transparent" style="border:1vh solid transparent; aspect-ratio: 1 / 1">');
+            var sq_cell = $('<th scope="col" class="bg-transparent">');
 
             for(var i=1; i<4; i++){ // minor square row
                 var row = $('<tr class="bg-transparent">');
                 
                 for(var j=1; j<4; j++){ // minor square col
-                    var cell = $('<th scope="col" class="bg-transparent" style="aspect-ratio: 1 / 1">');
-                    var field = $('<input id="'+[3*g+i, 3*h+j].toString()+'"style="height:7vh; font-size: 3.5vh; border-radius: 0;" class="form-control text-center" maxlength="1" oninput="this.value=this.value.replace(/[^1-9]/g,'+'\'\''+');">');
+                    var cell = $('<th scope="col" class="bg-transparent" style="border:0.5vh solid transparent;">');
+                    var field = $('<input id="'+[3*g+i, 3*h+j].toString()+'"style="aspect-ratio: 1 / 1; width: 7.5vmin; height:7.5vmin; font-size: 4vmin; border-radius: 0;" class="form-control text-center" maxlength="1" oninput="this.value=this.value.replace(/[^1-9]/g,'+'\'\''+');">');
                     cell.append(field);
                     row.append(cell);
                 }
@@ -26,5 +26,5 @@ $(document).ready(function(){
         table.append(sq_row)
     }
     // Appending the table to the HTML body
-    $('div.container').append(table);
+    $('div.container-fluid:first').append(table);
 });
